8733c95ac3b297560addd648b933a62ec217dea8
pranav <pranavbavaskar1@gmail.com>
2025-11-23 23:25:13 +0530

    added basic apis

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
 import express from "express";
+import router from "./api/routes/notification_routes";
+import { config } from "./config/env";
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("running on 3000");
+// will not read body if we dont use this middleware
+app.use(express.json());
+
+app.use("/api/v1/", router);
+
+const PORT = process.env.PORT;
+app.listen(config.port, () => {
+  console.log(`Server running on ${PORT || 3001}`);
 });
